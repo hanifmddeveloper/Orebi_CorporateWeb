7aceb86f953450fd8b96a6488ff576523c19ed35
hanifmddeveloper <hanifmddeveloper@gmail.com>
2025-09-08 22:24:41 +0600

    for sir presention

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -13,24 +13,39 @@ const Post = ({allPage,filterShow,listItem}) => {
    // console.log(filterShow);
    // console.log(listItem);
    let [cateFilterShow,setCateFilterShow]= useState([])
+   let [show,setShow] = useState(true)
 
    useEffect(()=>{
     let cateAll = filterShow.slice(0,5)
     setCateFilterShow(cateAll)
    },[filterShow])
+
+   let handleShow = ()=>{
+    setCateFilterShow(filterShow)
+    setShow(false)
+   }
+
+   let handleLess = ()=>{
+    let cateAll = filterShow.slice(0,5)
+    setCateFilterShow(cateAll)
+    setShow(true)
+   }
     
   return (
     <>
     <div className=' flex flex-wrap justify-between' >
-      {cateFilterShow.length > 0 ? (
-          cateFilterShow.map((item) => (
-            <div className="w-[32%]">
+     {cateFilterShow.length > 0 ?
+    <div className=''>
+      <div className='flex flex-wrap justify-between' >
+     {cateFilterShow.map((item) => (
+             <div className={`${cateFilterShow.length == 1 ? 'w-full ml-10' : 'w-[32%]'}`}>
+             {/* <div className="w-[32%]"> */}
               <div className="relative group">
                 <Link to={`/product/${item.id}`}>
-                  <img className="bg-[#D8D8D8]" src={item.thumbnail} alt="" />
+                  <img className="bg-[#D8D8D8] w-full" src={item.thumbnail} alt="" />
                 </Link>
                 <div className=" absolute bottom-0 left-0 w-full opacity-0 group hover:opacity-100">
-                  <ul className="bg-[#262626] pr-3 pt-4 pb-4 py-2 w-[94.5%] cursor-pointer shadow-2xs">
+                  <ul className="bg-[#262626] pr-3 pt-4 pb-4 py-2 cursor-pointer shadow-2xs w-full">
                     <li className="flex justify-end items-center  gap-2">
                       <span className="text-white">Add to Wish List</span>
                       <FaHeart className="text-[red]" />
@@ -46,26 +61,34 @@ const Post = ({allPage,filterShow,listItem}) => {
                   </ul>
                 </div>
               </div>
-              <div className="flex justify-between mx-auto shadow-2xl border-1 border-[#4800ff1d] bg-[#716f6f] w-[94%] mr-4.5">
+              <div className="flex items-center justify-between shadow-2xl border-1 border-[#434040] bg-[#6c6969]">
                 <div className="">
                   <h3>{item.title}</h3>
                   <p>Black</p>
                 </div>
                 <div className="">
                   <h3 className="pl-3">${item.price}</h3>
-                  <p className="pr-4 text-[red]">Stock {item.stock}</p>
+                  <p className=" text-[red]">Stock {item.stock}</p>
                 </div>
               </div>
             </div>
-          )))
-        : <div className={`${listItem == 'active' ? 'w-full' : 'w-full flex flex-wrap'}`} >
+            ))}
+          </div>
+          {filterShow.length > 5 && 
+          show ?
+          <h2 onClick={handleShow} className='mt-3 border-1 border-[#5f5d5d] inline-block px-2 py-1 bg-[gray] cursor-pointer' >Show All</h2> 
+          : filterShow.length > 5 && 
+          <h2 onClick={handleLess} className='mt-3 border-1 border-[#858383] inline-block px-7 py-1 bg-[gray] cursor-pointer' >Less</h2> }
+        
+    </div>
+        : <div className={`${listItem == 'active' ? 'w-full' : 'w-full flex justify-between flex-wrap'}`} >
           {allPage.map((item) => (
               <div className="w-[32%] ">
-                <div className="relative group">
+                <div className="relative group ">
                   <Link to={`/product/${item.id}`}>
-                    <img className="bg-[#D8D8D8]" src={item.thumbnail} alt="" />
+                    <img className="bg-[#D8D8D8] w-full" src={item.thumbnail} alt="" />
                   </Link>
-                  <div className="absolute bottom-0 left-0 w-[94.5%] transform translate-y-full opacity-0 transition-all duration-900 group-hover:translate-y-0 group-hover:opacity-100 ">
+                  <div className="absolute bottom-0 left-0 w-full transform translate-y-full opacity-0 transition-all duration-900 group-hover:translate-y-0 group-hover:opacity-100 ">
                     <ul className="bg-[rgba(39,37,37,0.7)] pr-3 pt-4 pb-4 py-2 shadow-2xl border-1 border-[#4800ff1d]">
                       <li className="flex justify-end items-center gap-4">
                         <span className="text-white">Add to Wish List</span>
@@ -82,7 +105,7 @@ const Post = ({allPage,filterShow,listItem}) => {
                     </ul>
                   </div>
                 </div>
-                <div className="flex justify-between mx-auto bg-[rgb(160,158,158)] w-[94.5%] mr-5">
+                <div className="flex items-center justify-between bg-[rgb(137,132,132)] w-full shadow-2xl">
                   <div className="">
                     <h3 className="text-[13px]">{item.title}</h3>
                     <p>Black</p>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -12,20 +12,24 @@ import { Link } from 'react-router';
 const Product = () => {
 
 let info = useContext(ApiData)
- console.log(info);
+// console.log(info);
   let [perPage, setPerPage] = useState(6)
   let [currentPage, setCurrentPage] = useState(1)
   let[categoryp,setCategoryp] = useState([])
   let [filterShow,setFilterShow] = useState([])
   let [listItem,setListItem]= useState("")
+  let [active, setActive] = useState("grid");
   let lastPage = perPage * currentPage
   let firstPage = lastPage - perPage
-  let allPage = info.slice(firstPage,lastPage)   // so now we send allPage insted of info
+  
+  let allPage = info.slice(firstPage,lastPage)
  console.log(allPage);
+
+ let [brand,setBrand]= useState([])
 
 
 let pageNumber = []
-  for(let i=0; i< Math.ceil(filterShow.length > 0 ? filterShow : info.length / perPage);i++){
+  for(let i=0; i< Math.ceil(filterShow.length > 0 ? filterShow.length : info.length / perPage);i++){
    pageNumber.push(i)
   }
 // console.log(pageNumber); 
@@ -53,6 +57,7 @@ let handlePerPageChange =(e)=>{
 }
 useEffect(()=>{
 setCategoryp([...new Set(info.map((item)=>item.category))])
+setBrand([...new Set(info.map((item)=>item.brand))])
 },[info])
 
 let handalCategory = (citem)=>{
@@ -60,6 +65,7 @@ let handalCategory = (citem)=>{
 let cateFilter = info.filter((item)=>item.category == citem)
 //console.log("ami",citem);
 setFilterShow(cateFilter)
+
 //console.log(cateFilter);
 }
 let handleAll = ()=>{
@@ -71,7 +77,15 @@ let handleListItem = ()=>{
  setListItem("active")
 }
 
-//console.log(listItem);
+let handleBrand = (bitem)=>{
+  //console.log("ami");
+  let brandFilter = info.filter((item)=> item.brand == bitem)
+//  console.log(brandFilter);
+setFilterShow(brandFilter);
+  
+}
+
+//console.log(brand);
 
 
   return (
@@ -82,21 +96,29 @@ let handleListItem = ()=>{
         <p><Link to="/">Home</Link> / <Link to="/product">Product</Link></p>
         </div>
         <div className="flex justify-between" >
-         <div className='w-[20%]' >
-          <h2 className='text-[#262626] font-dm text-[20px] font-bold' >Shop by Category</h2>
-          <ul>
-          <li onClick={handleAll} className='font-dm text-[16px] text-[#262626] font-normal' >All Products</li>
-        </ul>
+         <div className='w-[22%]'>
+          <div className='py-2 px-3 border border-[#262626] shadow-2xl' >
+           <h2 className='text-[#262626] font-dm text-[20px] font-bold' >Shop by Category</h2>
           <ul className='' >
-            {categoryp.map((item)=>(
-              <li onClick={()=>handalCategory(item)} className='text-[#262626] font-dm text-[16px] font-normal' >{item}({item.length})</li>
-             ))}
+          <li onClick={handleAll} className='font-dm text-[16px] text-[#262626] font-normal' >All Products</li>
+          {categoryp.map((item)=>(
+          <li onClick={()=>handalCategory(item)} className='text-[#262626] font-dm text-[16px] font-normal' >{item}({item.length})</li>
+           ))}
           </ul>
           </div>
+          <div className='py-2 px-3 border border-[#262626] shadow-2xl w-full' >
+            <h2 className='text-[#262626] font-dm text-[20px] font-bold mt-2 w-full' >Brand</h2>
+            <ul>
+              {brand.map((item)=>(
+              <li onClick={()=>handleBrand(item)} className='w-full'>{item}</li>
+              ))}
+            </ul>
+          </div>
+          </div>
           
-          <div className='w-[80%]' >
-          <div className='flex justify-between' >
-           <div className="flex gap-5" >
+          <div className='w-[78%]' >
+            <div className='flex items-center justify-between' >
+          <div className="flex gap-3" >
             <div onClick={()=>setListItem("")} className={`${listItem == "active" ? "" : "bg-green-500"} p-3 rounded-[3px]`}>
             <FaBorderAll />
             </div>
@@ -104,18 +126,17 @@ let handleListItem = ()=>{
             <ImListNumbered /> 
             </div>
             </div>
-            
-          <div className=' flex  items-center ml-[350px]' >
-          <labil className='pr-2' htmlFor="">Feature by:</labil>
-           <select name='' id='' className='py-1 px-3 border border-[#262626]' >
+          <div className='flex justify-between' >
+           <div className='pr-3' >
+           <labil className='mr-3' htmlFor="">Feature by :</labil>
+           <select name='' id='' className='py-1 px-2 border border-[#262626]' >
            <option value='3'>Price</option>
            <option value='6'>High to Low</option>
            <option value='9'>Low to High</option>
-           
-          </select>
-          </div>
-          <div className='flex  items-center mr-[60px]' >
-          <labil className='pr-2' htmlFor="">Show:</labil>
+           </select>
+           </div>
+           <div className='pr-3 ' >
+          <labil className='mr-3' htmlFor="">Show :</labil>
            <select onChange={handlePerPageChange} name='' id='' className='py-1 px-3 border border-[#262626]' >
            <option value='3'>3</option>
            <option value='6'>6</option>
@@ -124,11 +145,14 @@ let handleListItem = ()=>{
           </select>
           </div>
           </div>
+          </div>
           <Post allPage ={allPage} filterShow={filterShow} listItem={listItem} />
-          <Pagination pageNumber={pageNumber} paginate={paginate} next={next} previous={previous} currentPage={currentPage}/>
+          {((filterShow.length === 0 && pageNumber.length > 1) || (filterShow.length > 1 && Math.ceil(filterShow.length / perPage) > 1)) && (
+            <Pagination pageNumber={pageNumber} paginate={paginate} next={next} previous={previous} currentPage={currentPage}/>
+          )}
           </div>
-          </div>
-       </Container>
+       </div>
+      </Container>
     </section>
   )
 }
